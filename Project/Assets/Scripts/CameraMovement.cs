50f27bef722b7e005266b47470daa084ae7fbfb7
Pau Blanes <paublanespardo@enti.cat>
2018-11-06 20:25:04 +0100

    merge

--- Project/Assets/Scripts/CameraMovement.cs
+++ Project/Assets/Scripts/CameraMovement.cs
@@ -8,6 +8,8 @@ public class CameraMovement : MonoBehaviour {
 
     private Camera cam;
 
+    public int speed;
+
     // Use this for initialization
     void Start () {
 
@@ -21,15 +23,15 @@ public class CameraMovement : MonoBehaviour {
         GetInputs();
 
         if (mousePosition.y > cam.orthographicSize - 1) {
-            movement.y = transform.position.y + 10 * Time.deltaTime;
+            movement.y = transform.position.y + speed * Time.deltaTime;
         }else if (mousePosition.y < -(cam.orthographicSize - 1)) {
-            movement.y = transform.position.y - 10 * Time.deltaTime;
+            movement.y = transform.position.y - speed * Time.deltaTime;
         }
 
         if (mousePosition.x > cam.orthographicSize * 2) {
-            movement.x = transform.position.x + 10 * Time.deltaTime;
+            movement.x = transform.position.x + speed * Time.deltaTime;
         } else if (mousePosition.x < -(cam.orthographicSize * 2)) {
-            movement.x = transform.position.x - 10 * Time.deltaTime;
+            movement.x = transform.position.x - speed * Time.deltaTime;
         }
 
         transform.position = new Vector3(movement.x, movement.y, transform.position.z);
