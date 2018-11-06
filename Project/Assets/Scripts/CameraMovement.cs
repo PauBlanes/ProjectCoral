55b86ac6abf0bbe2c0d51b8fdadaaf2bcdf2af56
Edu-gamo <eduard_gm94@hotmail.com>
2018-11-06 20:20:21 +0100

    Movimiento Camara
    Movimiento de la camara con el raton
    Falta bloquear al llegar a los limites del mapa

---
+++ Project/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    private Vector2 mousePosition, movement;
+
+    private Camera cam;
+
+    // Use this for initialization
+    void Start () {
+
+        cam = GetComponent<Camera>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        GetInputs();
+
+        if (mousePosition.y > cam.orthographicSize - 1) {
+            movement.y = transform.position.y + 10 * Time.deltaTime;
+        }else if (mousePosition.y < -(cam.orthographicSize - 1)) {
+            movement.y = transform.position.y - 10 * Time.deltaTime;
+        }
+
+        if (mousePosition.x > cam.orthographicSize * 2) {
+            movement.x = transform.position.x + 10 * Time.deltaTime;
+        } else if (mousePosition.x < -(cam.orthographicSize * 2)) {
+            movement.x = transform.position.x - 10 * Time.deltaTime;
+        }
+
+        transform.position = new Vector3(movement.x, movement.y, transform.position.z);
+
+    }
+
+    void GetInputs() {
+
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+
+    }
+}
