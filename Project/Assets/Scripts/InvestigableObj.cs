0295ad2ee2f38fcee5f652fb68d0191997bd17e7
Pau Blanes <paublanespardo@enti.cat>
2018-11-06 16:17:15 +0100

    Robot investigacion i bases de spawn robots

---
+++ Project/Assets/Scripts/InvestigableObj.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvestigableObj : MonoBehaviour {
+
+    private RobotManager rB;
+
+	// Use this for initialization
+	void Start () {
+        rB = Camera.main.GetComponent<RobotManager>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//pintar aura quan toqui
+	}
+
+    private void OnMouseDown()
+    {
+        print(rB.iRobotSelected);
+      if(rB.iRobotSelected)
+        {
+            rB.SpawnRobot(transform, Vector3.zero);
+        }
+    }
+}
---
+++ Project/Assets/Scripts/RobotManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotManager : MonoBehaviour {
+    private GameObject robot2Spawn;
+
+    public GameObject[] possibleITargets; //targets for the investigation robot
+    public bool iRobotSelected;
+
+    private bool robotSelected;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public void SelectRobot(GameObject robot)
+    {
+        robot2Spawn = robot;
+        if (robot2Spawn.name == "InvestigationRobot")
+        {
+            iRobotSelected = true;
+        }
+    }
+    public void SpawnRobot (Transform t, Vector3 o)
+    {
+        GameObject newRobot = Instantiate(robot2Spawn, Vector3.zero ,Quaternion.identity);
+        if (robot2Spawn.name == "InvestigationRobot")
+        {
+            newRobot.GetComponent<RobotInvest>().target = t;
+            iRobotSelected = false;
+        }
+
+        
+        
+    }
+}
