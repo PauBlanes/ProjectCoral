3ece80433e14d9a12557a496a952544f1c3915ec
Edu-gamo <eduard_gm94@hotmail.com>
2018-11-12 19:53:49 +0100

    Movimiento camara con limite de movimiento

--- Project/Assets/Scripts/CameraMovement.cs
+++ Project/Assets/Scripts/CameraMovement.cs
@@ -8,12 +8,25 @@ public class CameraMovement : MonoBehaviour {
 
     private Camera cam;
 
+    private float camWidth, camHeight, maxX, minX, maxY, minY;
+
     public int speed;
+
+    public SpriteRenderer sprRenderer;
 
     // Use this for initialization
     void Start () {
 
         cam = GetComponent<Camera>();
+
+        camHeight = cam.orthographicSize * 2f;
+        camWidth = camHeight * cam.aspect;
+
+        maxX = sprRenderer.size.x / 2 - camWidth / 2;
+        minX = -(sprRenderer.size.x / 2) + camWidth / 2;
+
+        maxY = sprRenderer.size.y / 2 - camHeight / 2;
+        minY = -(sprRenderer.size.y / 2) + camHeight / 2;
 
     }
 	
@@ -22,19 +35,30 @@ public class CameraMovement : MonoBehaviour {
 
         GetInputs();
 
-        if (mousePosition.y > cam.orthographicSize - 1) {
-            movement.y = transform.position.y + speed * Time.deltaTime;
-        }else if (mousePosition.y < -(cam.orthographicSize - 1)) {
-            movement.y = transform.position.y - speed * Time.deltaTime;
+        if (mousePosition.y > camHeight/2 - 1)
+        {
+            movement.y = speed * Time.deltaTime;
+        }
+        else if (mousePosition.y < -(camHeight/2 - 1))
+        {
+            movement.y = -speed * Time.deltaTime;
         }
 
-        if (mousePosition.x > cam.orthographicSize * 2) {
-            movement.x = transform.position.x + speed * Time.deltaTime;
-        } else if (mousePosition.x < -(cam.orthographicSize * 2)) {
-            movement.x = transform.position.x - speed * Time.deltaTime;
+        if (mousePosition.x > camWidth/2 - 1)
+        {
+            movement.x = speed * Time.deltaTime;
         }
+        else if (mousePosition.x < -(camWidth/2 - 1))
+        {
+            movement.x = -speed * Time.deltaTime;
+        }
+
+        movement = new Vector2(Mathf.Clamp(transform.position.x + movement.x, minX, maxX), 
+            Mathf.Clamp(transform.position.y + movement.y, minY, maxY));
 
         transform.position = new Vector3(movement.x, movement.y, transform.position.z);
+
+        movement = Vector2.zero;
 
     }
 
